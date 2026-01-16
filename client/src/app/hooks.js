52da014c9e05843b960ec23c7d86f07591c377c3
Aaditya Revandkar <aadityarevandkar@gmail.com>
2026-01-16 17:49:40 +0530

    Delete client directory

--- client/src/app/hooks.js
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-
-// Create typed hooks for better development experience
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = useSelector;
